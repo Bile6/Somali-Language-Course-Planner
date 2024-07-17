2c803d197716c04bf5c3b2a464a0687160958d95
Bile6 <bileabdulkadirmohamed@gmail.com>
2024-07-17 11:31:57 +0300

    adding some colors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="container mx-auto p-4">
+    <div className="bg-gray-200 container mx-auto p-4 rounded">
       <h2 className="text-2xl font-bold mb-4">About</h2>
       <p>Welcome to Somali language courses.</p>
     </div>
